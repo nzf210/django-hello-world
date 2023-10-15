9d4fe81a9bc80eb8c31e32db0f1295bb2751e1ea
Syahril S <syahrilsymd@gmail.com>
2023-10-16 05:38:04 +0800

    add build_files.sh

---
+++ build_files.sh
@@ -0,0 +1,6 @@
+# mkdir -p staticfiles_build/static
+# mkdir -p main/static
+# pip install -r requirements.txt
+python3.9 main/manage.py makemigrations 
+python3.9 main/manage.py migrate 
+# python3.9 main/manage.py collectstatic --noinput
